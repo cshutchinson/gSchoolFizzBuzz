141e72a8ce379cdce0ebe9b641405bd993a848da
Christopher Hutchinson <cshutchinson@gmail.com>
2015-10-27 09:12:19 -0600

    final implementation

---
+++ fb.js
@@ -0,0 +1,6 @@
+for (var x=1; x<=30; x++){
+  if (x%15===0) console.log("Fizz Buzz") ;
+  else if (x%3===0) console.log ("Fizz");
+  else if (x%5===0) console.log ("Buzz");
+  else if (x%3!=0 && x%5!=0) console.log(x);
+}
