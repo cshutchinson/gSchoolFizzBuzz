c3a5698769325d49f055a71b4841a806f38c479f
Christopher Hutchinson <cshutchinson@gmail.com>
2015-10-29 10:06:44 -0600

    tests in place and passing

--- fb.js
+++ fb.js
@@ -1,11 +1,13 @@
 module.exports = {
   fizzBuzz: function(n){
+    var results=[];
     for (var x=0; x<=n; x++){
-      if (x%15===0) console.log("Fizz Buzz") ;
-      else if (x%3===0) console.log ("Fizz");
-      else if (x%5===0) console.log ("Buzz");
-      else if (x%3!=0 && x%5!=0) console.log(x);
+      if (x===0) results.push(0);
+      else if (x%15===0) results.push("Fizz Buzz") ;
+      else if (x%3===0) results.push ("Fizz");
+      else if (x%5===0) results.push ("Buzz");
+      else if (x%3!=0 && x%5!=0) results.push(x);
     }
-    return n;
+    return results;
   }
 }
--- test/test.js
+++ test/test.js
@@ -1,8 +1,17 @@
 var code = require('../fb.js');
 var expect = require('chai').expect;
 
-describe ('test fb function', function(){
-  it('Should return array of results', function(){
-    expect(code.fizzBuzz(1)).to.equal(1);
+describe ('FizzBuzz returns array', function(){
+  it('Should return an array when passed any value', function(){
+    expect(typeof code.fizzBuzz(Math.random()*100)).to.equal('object');
   });
+  it('Should return 1 as the last array value when passed 1', function(){
+    expect(code.fizzBuzz(1)[1]).to.equal(1);
+  });
+  it('Should return Fizz as the last array value when passed 3', function(){
+    expect(code.fizzBuzz(3)[3]).to.equal('Fizz');
+  })
+  it('Should return FizzBuzz as the last array value when passed 300', function(){
+    expect(code.fizzBuzz(300)[300]).to.equal('Fizz Buzz');
+  })
 });
